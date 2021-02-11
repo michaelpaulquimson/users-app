7bc9b8b12b6c4ef8f8c5b8c36ddf2ed48f803833
Mikee Quimson <michael.quimson@axa.com.ph>
2021-02-11 16:28:33 +0800

    Update UsersContainer.js

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import '../App.css';
 import styled from 'styled-components';
 
 import UsersService from '../services/UsersService';
+import UserList from '../components/UserList';
+import CreateUser from '../components/CreateUser';
 
 const Header = styled.h1`
 	margin: 25px;
@@ -13,7 +16,6 @@ function App() {
 
 	useEffect(() => {
 		getUsers();
-		console.log(users);
 	}, []);
 
 	async function getUsers() {
@@ -21,9 +23,44 @@ function App() {
 		if (users) setUsers(users);
 	}
 
+	async function createUser(user) {
+		const userToCreate = {
+			...user,
+			id: users.length + 1
+		};
+		await UsersService.createUser(userToCreate);
+		setUsers([
+			...users,
+			userToCreate
+		]);
+	}
+
+	async function updateUser(data) {
+		await UsersService.updateUser(data);
+		setUsers(users.map(u => {
+			if (u.id === data.id) {
+				return data;
+			}
+			return u;
+		}));
+	}
+
+	async function deleteUser(id) {
+		await UsersService.deleteUser(id);
+		setUsers(users.filter(u => u.id !== id));
+	}
+
 	return (
 		<div>
 			<Header>User Management System</Header>
+			<CreateUser
+				onCreate={ async (user) => await createUser(user)}
+			/>
+			<UserList
+				users={users}
+				onUpdate={ async (user) => await updateUser(user)}
+				onDelete={ async (user) => await deleteUser(user)}
+			/>
 		</div>
 	);
 }
