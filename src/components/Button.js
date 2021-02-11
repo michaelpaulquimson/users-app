c8d5dbb508b04ad90dcbe46a126084acf38043db
Mikee Quimson <michael.quimson@axa.com.ph>
2021-02-11 14:05:53 +0800

    Create Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+	border: 1px solid black;
+	margin: 8px;
+	padding: 4px;
+	border-radius: 4px;
+	width: 280px;
+	text-align: center;
+	background-color: #4CAF50;
+	color: white;
+`;
+
+const Button = ({ type, name }) => {
+	return (
+		<StyledButton type={type}>{name}</StyledButton>
+	);
+};
+
+Button.propTypes = {
+	type: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired
+};
+
+export default Button;
