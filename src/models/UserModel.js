f9d6e16b627abee6fd7ded016b035b3403e97bcf
Mikee Quimson <michael.quimson@axa.com.ph>
2021-02-11 16:16:52 +0800

    Update UserModel.js

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,10 +1,12 @@
+import Utils from '../utils/Utils';
+
 class User {
 	constructor({ id, firstName, lastName, email, dob }) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-		this.dob = dob;
+		this.dob = Utils.convertToDate(dob);
 	}
 }
 
