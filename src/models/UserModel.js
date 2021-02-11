212420fec0fe7874677d4f19547cc268ff33ecf6
Mikee Quimson <michael.quimson@axa.com.ph>
2021-02-11 13:16:17 +0800

    Create UserModel.js

---
+++ src/models/UserModel.js
@@ -0,0 +1,11 @@
+class User {
+	constructor({ id, firstName, lastName, email, dob }) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.dob = dob;
+	}
+}
+
+export default User;
