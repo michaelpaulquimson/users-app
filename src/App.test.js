04625e9011095dca63b23cddc723b703e8cecdf7
Mikee Quimson <michael.quimson@axa.com.ph>
2021-02-12 10:22:19 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+test('renders react app', () => {
+
+	const { queryAllByTitle } = render(<App />);
+	const app = queryAllByTitle('app');
+
+	expect(app).toBeTruthy();
 });
