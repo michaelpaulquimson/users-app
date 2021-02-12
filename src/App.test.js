be7dec6cff07e3250d4508ad65da3093aa3abdaf
Mikee Quimson <michael.quimson@axa.com.ph>
2021-02-12 10:29:14 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders react app', () => {
-
 	const { queryAllByTitle } = render(<App />);
 	const app = queryAllByTitle('app');
 
